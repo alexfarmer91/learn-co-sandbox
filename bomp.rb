a903410eda644a51c94ae908857b513b4ef36841
Alex Farmer <farmale10@gmail.com>
2019-09-10 13:25:20 +0000

    Automatically backed up by Learn

---
+++ bomp.rb
@@ -0,0 +1,11 @@
+
+sports_cars = {
+  :awesome => {
+    :chevrolet => ["Corvette", "Camaro"],
+    :porsche => ["911","Cayman","944"]
+  }
+  :lame => {
+    :mazda => "Miata",
+    :subaru => "BRZ"
+  }
+}
